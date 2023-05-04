8f1a14ac6a64bada916b85988593fdaddd36c7e0
bishil06 <bishil06@naver.com>
2023-05-05 07:32:45 +0900

    input url

--- video-cutter-app/src/VideoSelector.js
+++ video-cutter-app/src/VideoSelector.js
@@ -1,4 +1,8 @@
+import { useState } from 'react'
+
 export default function VideoSelector({setVideoFile}) {
+    const [urlInput, setUrlInput] = useState()
+
     const handleChange = (e) => {
         console.log(e.target.files)
         if (e.target.files.length === 1) {
@@ -6,9 +10,14 @@ export default function VideoSelector({setVideoFile}) {
         }
     }
 
+    const urlHandle = (e) => {
+        setVideoFile({ path: urlInput})
+    }
+
     return (
         <div width="100%">
             <input type="file" accept="video/*" onChange={handleChange} style={{width: '100%'}}/>
+            <label>웹주소</label><input type="url" onChange={(e) => setUrlInput(e.target.value)} value={urlInput}></input><button onClick={urlHandle}>입력</button>
         </div>
         
     )
